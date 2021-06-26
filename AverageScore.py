4a4694f65380cdc5a5451f4303ba5e1777389aed
ameenbaig <baigameen579@gmail.com>
2021-06-26 21:08:58 +0100

    Initial Commit contains the first draft of the AverageScore class.

---
+++ AverageScore.py
@@ -0,0 +1,14 @@
+#Defining the actual Average Score
+class AverageScore:
+
+    #Initializing method for class AverageScore
+    #This method difines the properties and populates them
+    def __init__(self, maths_marks, english_marks, physics_marks):
+        self.maths_marks = maths_marks
+        self.english_marks = english_marks
+        self.physics_marks = physics_marks
+
+    #Calculates the average score for the 3 subjects
+    def calculate_avg_score(self):
+        avg_score = (self.maths_marks + self.english_marks + self.physics_marks)/3
+        return avg_score
