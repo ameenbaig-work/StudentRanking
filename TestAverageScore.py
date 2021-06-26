04e2cdc5d009ead51d2ac41e8e6318f2e34bf9cf
ameenbaig <baigameen579@gmail.com>
2021-06-26 22:58:59 +0100

    Introduced an assertRaises error to check if errors are raised when a negative value is entered.

--- TestAverageScore.py
+++ TestAverageScore.py
@@ -2,9 +2,15 @@ import unittest
 from AverageScore import *
 
 class TestAverageScore(unittest.TestCase):
+
+    def test__init__(self):
+        self.assertRaises(TypeError, AverageScore.__init__(-50, 60, 70), )
+
     def test_calculate_avg_score(self):
         student_marks = AverageScore(50,60,70)
-        self.assertEqual(student_marks.calculate_avg_score(),60,"Average Score Not Calculated Correctly")
+        self.assertEquals(student_marks.calculate_avg_score(), 60, "Average Score Not Calculated Correctly")
+        self.assertAlmostEquals(student_marks.calculate_avg_score(), 60, 1, "Average Score Not Calculated Correctly")
+    #   self.assertNotEqual(student_marks.calculate_avg_score(),60,"")
 
 if __name__ == '__main__':
     unittest.main()
