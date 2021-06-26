353dbc133ea4c14b532c414481405a776eb2e9e2
ameenbaig <baigameen579@gmail.com>
2021-06-26 22:08:41 +0100

    Added a class variable max_marks to store the maximum marks obtainable in a subject.

--- AverageScore.py
+++ AverageScore.py
@@ -4,6 +4,7 @@ class AverageScore:
     #Initializing method for class AverageScore
     #This method difines the properties and populates them
     def __init__(self, maths_marks, english_marks, physics_marks):
+        self.max_marks= 100
         self.maths_marks = maths_marks
         self.english_marks = english_marks
         self.physics_marks = physics_marks
