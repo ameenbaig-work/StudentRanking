53994782fe01f9b8b5bba21dd7009bb85e11848e
ameenbaig <baigameen579@gmail.com>
2021-06-28 10:04:14 +0100

    Added more tests in the TestCase for ProgressScore and restructured the code so that all tests are performed when calculating ProgressScore.

--- ProgressScore.py
+++ ProgressScore.py
@@ -3,20 +3,35 @@ class ProgressScore:
 
     #Initializing method for class ProgressScore
     #This method defines the properties and populates them
-    def __init__(self, actual_maths_marks, actual_english_marks, actual_physics_marks, mock_maths_marks, mock_english_marks, mock_physics_marks):
+    def __init__(self, mock_maths_marks, actual_maths_marks, mock_english_marks, actual_english_marks, mock_science_marks, actual_science_marks):
         self.max_marks = 100
+        self.validate_input_values(mock_maths_marks, actual_maths_marks, mock_english_marks, actual_english_marks, mock_science_marks, actual_science_marks)
         self.actual_maths_marks = actual_maths_marks
         self.actual_english_marks = actual_english_marks
-        self.actual_physics_marks = actual_physics_marks
+        self.actual_science_marks = actual_science_marks
         self.mock_maths_marks = mock_maths_marks
         self.mock_english_marks = mock_english_marks
-        self.mock_physics_marks = mock_physics_marks
+        self.mock_science_marks = mock_science_marks
+
+    def validate_input_values(self, mock_maths_marks, actual_maths_marks, mock_english_marks, actual_english_marks, mock_science_marks, actual_science_marks):
+        self.validate_input_size(mock_maths_marks, actual_maths_marks, mock_english_marks, actual_english_marks, mock_science_marks, actual_science_marks)
+        self.validate_input_datatype(mock_maths_marks, actual_maths_marks, mock_english_marks, actual_english_marks, mock_science_marks, actual_science_marks)
+
+    def validate_input_size(self, mock_maths_marks, actual_maths_marks, mock_english_marks, actual_english_marks, mock_science_marks, actual_science_marks):
+        if (mock_maths_marks > self.max_marks) or (actual_maths_marks > self.max_marks) or (mock_english_marks > self.max_marks) or \
+                (actual_english_marks > self.max_marks) or (mock_science_marks > self.max_marks) or (actual_science_marks > self.max_marks):
+            raise ValueError("Scores cannot exceed 100 marks")
+
+    def validate_input_datatype(self, mock_maths_marks, actual_maths_marks, mock_english_marks, actual_english_marks, mock_science_marks, actual_science_marks):
+        if isinstance(mock_maths_marks,str) or isinstance(actual_maths_marks,str) or isinstance(mock_english_marks,str) \
+                or isinstance(actual_english_marks,str) or isinstance(mock_science_marks,str) or isinstance(actual_science_marks,str):
+            raise ValueError("Input cannot be a string")
 
     #Calculates the average progress score for the 3 subjects
     def calculate_avg_progress_score(self):
         m_progress_marks = (self.actual_maths_marks - self.mock_maths_marks)
         e_progress_marks = (self.actual_english_marks - self.mock_english_marks)
-        p_progress_marks = (self.actual_physics_marks - self.mock_physics_marks)
+        p_progress_marks = (self.actual_science_marks - self.mock_science_marks)
         avg_progress_score = (m_progress_marks + e_progress_marks + p_progress_marks)/ (10*3)
         avg_progress_score = round(avg_progress_score, 2)
         return avg_progress_score
--- TestProgressScore.py
+++ TestProgressScore.py
@@ -4,10 +4,15 @@ from ProgressScore import *
 class TestProgressScore(unittest.TestCase):
 
     def test_calculate_avg_progress_score(self):
-        student_marks = ProgressScore(88,96,54,99,92,79)
-        print(student_marks.calculate_avg_progress_score())
+        student_marks = ProgressScore(99, 88, 92, 96, 79, 54)
         self.assertAlmostEqual(student_marks.calculate_avg_progress_score(), -1.07, 2, "Average Score Not Calculated Correctly")
 
+    def test_check_input_size(self):
+        student_marks = ProgressScore(99, 88, 92, 96, 79, 54)
+        self.assertRaises(ValueError, student_marks.validate_input_size, 990, 88, 92, 96, 79, 54)
+
+    def test_check_input_datatype(self):
+        self.assertRaises(ValueError, ProgressScore.validate_input_datatype, self, "99", 88, 92, 96, 79, 54)
 
 if __name__ == '__main__':
     unittest.main()
