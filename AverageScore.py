8e356cc1fde638f0d55a139af13dbff46f79fbdc
ameenbaig <baigameen579@gmail.com>
2021-06-28 10:02:33 +0100

    Redesigned the code for AverageScore and TestAverageScore so that all tests are applied when calculating the average score.

--- AverageScore.py
+++ AverageScore.py
@@ -3,30 +3,34 @@ class AverageScore:
 
     #Initializing method for class AverageScore
     #This method difines the properties and populates them
-    def __init__(self, maths_marks, english_marks, physics_marks):
+    def __init__(self, maths_marks, english_marks, science_marks):
         self.max_marks = 100
+        self.validate_input_values(maths_marks, english_marks, science_marks)
         self.maths_marks = maths_marks
         self.english_marks = english_marks
-        self.physics_marks = physics_marks
+        self.science_marks = science_marks
 
-    def validate_input_values(self, maths_marks, english_marks, physics_marks):
-        if (maths_marks < 0) or (english_marks < 0) or (physics_marks < 0):
+    def validate_input_values(self, maths_marks, english_marks, science_marks):
+        self.validate_negative_input(maths_marks, english_marks, science_marks)
+        self.validate_input_size(maths_marks, english_marks, science_marks)
+        self.validate_input_datatype(maths_marks, english_marks, science_marks)
+
+    def validate_negative_input(self, maths_marks, english_marks, science_marks):
+        if (maths_marks < 0) or (english_marks < 0) or (science_marks < 0):
             print("before raising error")
             raise ValueError("Only positive values are allowed")
 
-    def validate_input_size(self, maths_marks, english_marks, physics_marks):
-        if (maths_marks > self.max_marks) or (english_marks > self.max_marks) or (physics_marks > self.max_marks):
+    def validate_input_size(self, maths_marks, english_marks, science_marks):
+        if (maths_marks > self.max_marks) or (english_marks > self.max_marks) or (science_marks > self.max_marks):
             raise ValueError("Scores cannot exceed 100 marks")
 
-    def validate_input_datatype(self, maths_marks, english_marks, physics_marks):
-        if isinstance(maths_marks,str) or isinstance(english_marks,str) or isinstance(physics_marks,str):
+    def validate_input_datatype(self, maths_marks, english_marks, science_marks):
+        if isinstance(maths_marks,str) or isinstance(english_marks,str) or isinstance(science_marks,str):
             raise ValueError("Input cannot be a string")
 
     #Calculates the average score for the 3 subjects
     def calculate_avg_score(self):
-        avg_score = (self.maths_marks + self.english_marks + self.physics_marks)/3
+        avg_score = (self.maths_marks + self.english_marks + self.science_marks)/3
+        avg_score = round(avg_score,2)
         return avg_score
-
-#s1 = AverageScore(50,60,70)
-#s1.validate_input_size(120, 60, 70)
 
--- TestAverageScore.py
+++ TestAverageScore.py
@@ -4,17 +4,18 @@ from AverageScore import *
 class TestAverageScore(unittest.TestCase):
 
     def test_validate_input_values(self):
-        self.assertRaises(ValueError, AverageScore.validate_input_values, self, -50, 60, 70)
+        self.assertRaises(ValueError, AverageScore.validate_negative_input, self, -88, 96, 54)
 
     def test_calculate_avg_score(self):
-        student_marks = AverageScore(50,60,70)
-        self.assertAlmostEquals(student_marks.calculate_avg_score(), 60, 2, "Average Score Not Calculated Correctly")
+        student_marks = AverageScore(88, 96, 54)
+        self.assertAlmostEqual(student_marks.calculate_avg_score(), 79.33, 2, "Average Score Not Calculated Correctly")
 
     def test_check_input_size(self):
-        self.assertRaises(ValueError, AverageScore.validate_input_size, self, 500, 60, 70)
+        student_marks = AverageScore(88, 96, 54)
+        self.assertRaises(ValueError, student_marks.validate_input_size, 880, 96, 54)
 
     def test_check_input_datatype(self):
-        self.assertRaises(ValueError, AverageScore.validate_input_datatype, self, "50", 60, 70)
+        self.assertRaises(ValueError, AverageScore.validate_input_datatype, self, "88", 96, 54)
 
 if __name__ == '__main__':
     unittest.main()
